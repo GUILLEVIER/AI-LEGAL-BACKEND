cb1baff96295be54e2cf8a8a0628d826c715942e
Juan Maldonado <jcmalco@gmail.com>
2025-07-25 17:19:01 -0400

    testing

--- backend/users/admin.py
+++ backend/users/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.admin import UserAdmin, GroupAdmin
 from .models import Usuarios
 from django.contrib.sites.models import Site
 from allauth.socialaccount.models import SocialToken, SocialAccount, SocialApp
@@ -7,8 +7,12 @@ from allauth.account.models import EmailAddress
 from rest_framework.authtoken.admin import TokenProxy
 from django.contrib.admin.sites import NotRegistered
 
+from django.contrib.auth.models import Group
+
 from unfold.admin import ModelAdmin
 from unfold.forms import AdminPasswordChangeForm, UserChangeForm, UserCreationForm
+
+admin.site.unregister(Group)
 
 
 class CustomUserAdmin(UserAdmin, ModelAdmin):
@@ -29,6 +33,10 @@ class CustomUserAdmin(UserAdmin, ModelAdmin):
 
 
 admin.site.register(Usuarios, CustomUserAdmin)
+
+@admin.register(Group)
+class GroupAdmin(GroupAdmin, ModelAdmin):
+    pass
 
 # Ocultar modelos no deseados del admin si están registrados
 for model in [TokenProxy, Site, EmailAddress, SocialToken, SocialAccount, SocialApp]:
