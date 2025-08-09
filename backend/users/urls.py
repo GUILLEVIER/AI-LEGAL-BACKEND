22b8909731ab2178d7c9935a95f3bb438a0acbd4
Juan Maldonado <jcmalco@gmail.com>
2025-08-09 18:45:40 -0400

    update filtrar por empresa, al crear asociar por empresa, editar y eliminar usuario de empresa, endpoint me

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,14 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UsuariosListAPIView, UsuarioDetailAPIView, GroupViewSet, UserPermissionsAPIView
+from .views import UsuariosViewSet, GroupAPIView
 
 router = DefaultRouter()
-router.register(r'groups', GroupViewSet, basename='groups')
+router.register(r'usuarios', UsuariosViewSet, basename='usuarios')
 
 urlpatterns = [
-    path('', UsuariosListAPIView.as_view(), name='usuarios-list'),
-    path('<int:usuario_id>/', UsuarioDetailAPIView.as_view(), name='usuario-detail'),
-    path('<int:usuario_id>/permissions/', UserPermissionsAPIView.as_view(), name='usuario-permissions'),
-    path('groups/', GroupViewSet.as_view(), name='group-list'),
-    #path('', include(router.urls)),
+    path('', include(router.urls)),
+    path('groups/', GroupAPIView.as_view(), name='group-list'),
 ]
